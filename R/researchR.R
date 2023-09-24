0d49340a3d16d362baab6f10abebc3f9178c00f8
Luke Peterson <luke.peterson@gmail.com>
2023-09-24 13:54:24 -0400

    Update researchR.R
    Added flip handler

--- R/researchR.R
+++ R/researchR.R
@@ -158,7 +158,7 @@ recenter_recode_vars <- function(x, existing_vars,
 #' @param recode_vars list of variable names to which we want to recode (must be same length and order as existing_vars)
 #' @param scale list of integer values for existing_vars, e.g. c(1:6) for 6-point scale from 1-6, c(18:22) for 5-point scale from 18-22
 #' @param na_value = 0 if there's a hard-coded response value we want to set to NA, indicate it here
-#' @param flip = FALSE logical indicating whether to flip the scale
+#' @param flip = FALSE logical indicating whether to flip the scale in case the higher numeric response value corresponds to the lower response
 #' @param low_appendage = "_no" the lower response
 #' @param high_appendage = "_yes" the higher response
 #' 
@@ -172,17 +172,7 @@ split_recode_nonnegative_vars <- function(x, existing_vars,
                                           flip = FALSE,
                                           low_appendage = "_no",
                                           high_appendage = "_yes") {
-  # testing variables:
-  # library(haven)
-  # x <- haven::read_spss("892579-data-Eggs_and_Syrup-09-11-2023-CX--Default.sav")
-  # existing_vars <- c('Q11_7',	'Q11_8',	'Q11_9',	'Q11_10',	'Q11_11',	'Q11_12',	'Q11_13',	'Q11_14',	'Q11_15',	'Q11_16',	'Q11_17',	'Q11_18',	'Q11_19',	'Q11_20',	'Q11_21',	'Q11_22',	'Q11_23',	'Q11_24',	'Q11_25',	'Q11_26',	'Q11_27',	'Q11_28',	'Q11_29',	'Q11_30',	'Q11_31',	'Q11_32',	'Q11_33',	'Q11_34',	'Q11_35',	'Q12_7',	'Q12_8',	'Q12_9',	'Q12_10',	'Q12_11',	'Q12_12',	'Q12_13',	'Q12_14',	'Q12_15',	'Q12_16',	'Q12_17',	'Q12_18',	'Q12_19',	'Q12_20',	'Q12_21',	'Q12_22',	'Q12_23',	'Q12_24',	'Q12_25',	'Q12_26',	'Q12_27',	'Q12_28',	'Q12_29',	'Q12_30',	'Q12_31',	'Q12_32',	'Q12_33',	'Q12_34',	'Q12_35',	'Q12_36',	'Q12_37',	'Q12_38',	'Q12_39',	'Q13_7',	'Q13_8',	'Q13_9',	'Q13_10',	'Q13_11',	'Q13_12',	'Q13_13',	'Q13_14',	'Q13_15',	'Q13_16',	'Q13_17',	'Q13_18',	'Q13_19',	'Q13_20',	'Q13_21',	'Q13_22',	'Q13_23',	'Q14_7',	'Q14_8',	'Q14_9',	'Q14_10',	'Q14_11',	'Q14_12',	'Q14_13',	'Q14_14',	'Q14_15',	'Q14_16',	'Q14_17',	'Q14_18',	'Q14_19',	'Q14_20',	'Q14_21',	'Q14_22',	'Q14_23',	'Q14_24',	'Q14_25',	'Q14_26',	'Q14_27',	'Q14_28',	'Q14_29',	'Q14_30',	'Q14_31',	'Q14_32',	'Q14_33',	'Q14_34',	'Q14_35',	'Q14_36',	'Q14_37',	'Q14_38',	'Q14_39',	'Q14_40',	'Q14_41',	'Q14_42',	'Q14_43')
-  # recode_vars <- c('buy_american',	'born_usa',	'parents_born_usa',	'i_recycle',	'i_vote_federal',	'i_vote_state',	'i_vote_local',	'go_to_work_sick',	'diet_informed',	'pay_for_environmentally_friendly_products',	'family_is_most_important',	'in_control_of_weight',	'existential_book_angst',	'spanish_in_home',	'english_in_home',	'nature_understanding',	'comfortable_being_unconventional',	'respect_customs_and_beliefs',	'exercises_regularly',	'sacrifice_family_for_career',	'breakfast_most_important',	'lunch_most_important',	'dinner_most_important',	'optimist',	'participate_in_protests',	'patronize_environmentally_friendly_companies',	'environmental_soundness_good_for_business',	'travel_abroad',	'prefers_travel_domestic',	'enjoys_cooking',	'tries_new_recipes',	'guilty_about_sweets',	'flavor_as_important_as_healthiness',	'no_guilt_over_fat_indulgence',	'eats_healthy',	'shops_organic_natural',	'not_price_sensitive_around_food',	'enjoys_foreign_foods',	'fast_food_is_junk',	'like_healthy_fast_food',	'eats_gourmet_food',	'snacks_healthy',	'nonartificial_preference',	'eats_precooked',	'local_eggs',	'organic_eggs',	'local_fruits_veggies',	'organic_fruits_veggies',	'local_meats',	'organic_meats',	'local_dairy',	'organic_dairy',	'local_organic_supports_local_farmers',	'local_organic_source_knowledge',	'local_organic_are_healthier',	'local_organic_carbon_footprint',	'farmers_markets',	'belongs_csa',	'avoids_sugar_unhealthy',	'avoids_sugar_unethical',	'prefers_non_sugar_artificial_sweeteners',	'prefers_natural_alternative_sugars',	'tries_new_drinks',	'buys_alcohol',	'collects_wine',	'winemaking_knowledge',	'restaurant_wine_chooser',	'collects_whiskey',	'whiskeymaking_knowledge',	'explores_beer',	'beermaking_knowledge',	'low_sugar_alcohol',	'low_carb_alcohol',	'low_cal_alcohol',	'na_beer',	'na_wine',	'na_cocktails',	'avoids_alcohol_unhealthy',	'fancy_alcohol_too_expensive',	'breakfast_syrup_user',	'breakfast_syrup_careful_user',	'breakfast_syrup_collector',	'breakfast_syrup_low_cal',	'breakfast_syrup_flavored_preference',	'table_syrup_user',	'pure_maple_user',	'pure_maple_souvenir',	'pure_maple_gift',	'pure_maple_buy_grocery',	'pure_maple_buy_farm_stand',	'pure_maple_buy_online',	'explores_breakfast_syrup',	'explores_pure_maple',	'pure_maple_discerning_palate',	'sugarmaking_knowledge',	'breakfast_syrup_mixology',	'breakfast_syrup_non_breakfast',	'pure_maple_mixology',	'pure_maple_non_breakfast',	'table_syrup_mixology',	'table_syrup_non_breakfast',	'enjoys_baking',	'baking_subs_pure_maple',	'table_syrup_prefers_local',	'maple_syrup_prefers_local',	'table_syrup_prefers_organic',	'pure_maple_prefers_organic',	'pure_maple_expensive_treat',	'pure_maple_unhealthy_treat',	'table_syrup_good_for_corn_farmers',	'pure_maple_good_for_sugarmakers',	'open_to_maple_subsidies',	'pure_maple_not_available',	'pure_maple_organic_not_available',	'pure_maple_local_not_available',	'american_pure_maple_preference')
-  # scale <- c(1:6)
-  # na_value = 0
-  # flip = FALSE
-  # low_appendage = "_no"
-  # high_appendage = "_yes"
-
+ 
   # make data_frame from vars (really just to throw an error if they're not the same length):
   var_frame <- data.frame(existing_vars <- existing_vars,
                           recode_vars <- recode_vars)
@@ -197,6 +187,13 @@ split_recode_nonnegative_vars <- function(x, existing_vars,
   #TODO: revise to include "zapping" function from haven
   if (na_value > 0) {
     target_data[, existing_vars][target_data[, existing_vars] == na_value] <- NA
+  }
+  
+  # handle flip, just reversing the string appendages:
+  if (flip == TRUE) {
+    tempvar <- low_appendage
+    low_appendage <- high_appendage
+    high_appendage <- tempvar
   }
   
   # determine center:
